34819d52889a0216af656e397c650bc210a31a96
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-08 03:14:48 +0330

    add style

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Dimensions, StyleSheet} from 'react-native';
+import Seacrh from '../../components/Search/Seacrh';
+
+const {height, width} = Dimensions.get('window');
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Seacrh />
+    </View>
+  );
+};
 
-export default Home
+export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: (4 * width) / 100,
+    paddingVertical: (height * 2) / 100,
+  },
+});
