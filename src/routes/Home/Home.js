48ce39ede930814e9cea60c6b7e36cf688eebe0e
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-12 01:21:07 +0330

    add flat list for test datas

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import {View, Dimensions, StyleSheet} from 'react-native';
+import {View, Dimensions, StyleSheet, FlatList,Text} from 'react-native';
 import Seacrh from '../../components/Search/Seacrh';
+import {useGetAllMovies} from '../../hooks';
 
 const {height, width} = Dimensions.get('window');
 
 const Home = () => {
+  const {data} = useGetAllMovies();
+  console.log(data);
   return (
     <View style={styles.container}>
       <Seacrh />
+      <FlatList
+        data={data?.results}
+        ListEmptyComponent={() => (
+          <View>
+            <Text>salam</Text>
+          </View>
+        )}
+        keyExtractor={(data, index) => index.toString()}
+        renderItem={({item, index}) => (
+          <View>
+            <Text>{item.overview}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
