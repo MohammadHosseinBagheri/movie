5c8188026141fc68481e2e679e2c70fd1f70bfc9
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-12 01:20:09 +0330

    add react-query

--- App.js
+++ App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import ConfigRoutes from './src/routes/Config';
-
+import {QueryClient, QueryClientProvider} from 'react-query';
 const App = () => {
-  return (<ConfigRoutes />
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+        retry:true,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ConfigRoutes />
+    </QueryClientProvider>
   );
 };
 
