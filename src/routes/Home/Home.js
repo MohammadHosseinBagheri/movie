7540bcbf365f5c595c9ebc1cad8b3f914796ebea
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-13 23:04:47 +0330

    add video container

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,30 +1,18 @@
 import React from 'react';
-import {View, Dimensions, StyleSheet, FlatList,Text} from 'react-native';
+import {View, Dimensions, StyleSheet, PixelRatio} from 'react-native';
+import CustomeError from '../../components/Error/Error';
+import VideoListContainer from '../../components/Home/VideoListContainer';
+import LoadingSpinner from '../../components/Loading/LoadingSpinner';
 import Seacrh from '../../components/Search/Seacrh';
 import {useGetAllMovies} from '../../hooks';
 
 const {height, width} = Dimensions.get('window');
 
 const Home = () => {
-  const {data} = useGetAllMovies();
-  console.log(data);
   return (
     <View style={styles.container}>
       <Seacrh />
-      <FlatList
-        data={data?.results}
-        ListEmptyComponent={() => (
-          <View>
-            <Text>salam</Text>
-          </View>
-        )}
-        keyExtractor={(data, index) => index.toString()}
-        renderItem={({item, index}) => (
-          <View>
-            <Text>{item.overview}</Text>
-          </View>
-        )}
-      />
+      <VideoListContainer />
     </View>
   );
 };
@@ -33,7 +21,7 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: (4 * width) / 100,
+    paddingHorizontal: (3 * width * PixelRatio.get()) / 100,
     paddingVertical: (height * 2) / 100,
   },
 });
