928a2e2acd4e38d7d0d06473c7bc459ddc61b021
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-13 23:04:00 +0330

    loading component

---
+++ src/components/Loading/LoadingSpinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {ActivityIndicator} from 'react-native';
+
+const LoadingSpinner = () => {
+  return <ActivityIndicator size="large" color="#546E7A" />;
+};
+
+export default LoadingSpinner;
