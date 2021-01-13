0258930d84105f216e8c3828bc1fc31e79f0bdba
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-13 23:02:33 +0330

    grid provider

--- App.js
+++ App.js
@@ -2,18 +2,21 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import ConfigRoutes from './src/routes/Config';
 import {QueryClient, QueryClientProvider} from 'react-query';
+import GridProvider from './src/context/grid/Grid.provider';
 const App = () => {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
         retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
-        retry:true,
+        retry: true,
       },
     },
   });
   return (
     <QueryClientProvider client={queryClient}>
-      <ConfigRoutes />
+      <GridProvider>
+        <ConfigRoutes />
+      </GridProvider>
     </QueryClientProvider>
   );
 };
