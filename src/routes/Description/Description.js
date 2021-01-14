173904848e560f43768c8e27391050c7deb664c0
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-15 00:05:59 +0330

    detaile screen

--- src/routes/Description/Description.js
+++ src/routes/Description/Description.js
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Icon} from 'native-base';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   Text,
@@ -9,6 +11,11 @@ import {
   PixelRatio,
   TouchableOpacity,
 } from 'react-native';
+import {SharedElement} from 'react-navigation-shared-element';
+import {getMovieDetails} from '../../apis';
+import LoadingSpinner from '../../components/Loading/LoadingSpinner';
+import {useGetAllMovies, useGetMovieDetails} from '../../hooks';
+import {getMoviesCache} from '../../hooks/react--query';
 const {width, height} = Dimensions.get('window');
 const Description = (props) => {
   const [isLimitLine, setLimitLine] = useState(true);
@@ -17,13 +24,44 @@ const Description = (props) => {
       params: {item},
     },
   } = props;
+  const id = item.id;
+  const navigation = useNavigation();
+  const [detail, setDetail] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getDetails = async (id) => {
+      setLoading(true);
+      const data = await getMovieDetails(id);
+      if (data) {
+        setDetail(data);
+        setLoading(false);
+      } else {
+        setLoading(true);
+      }
+    };
+    getDetails(id);
+  }, []);
   const handlePress = () => {
     setLimitLine((prevState) => !prevState);
   };
   return (
     <>
-      <Image style={styles.descriptionImage} source={{uri: item.poster_path}} />
-      <ScrollView style={styles.descriptionDetailsContainer}>
+      <SharedElement id={`item.${item.id}.image`}>
+        <Image
+          resizeMode={'stretch'}
+          style={styles.descriptionImage}
+          source={{
+            uri: `https://image.tmdb.org/t/p/original${item.poster_path}`,
+          }}
+        />
+      </SharedElement>
+      <TouchableOpacity
+        style={{position: 'absolute', left: 5, top: 0}}
+        onPress={() => navigation.goBack()}>
+        <Icon name="arrow-back-outline" style={{color: '#455A64'}} />
+      </TouchableOpacity>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.descriptionDetailsContainer}>
         <View style={styles.descriptionTitleContainer}>
           <Text style={styles.descriptionTitleText}>2h 3m</Text>
           <Text style={styles.descriptionTitleText}>Adventure / Crime</Text>
@@ -34,21 +72,58 @@ const Description = (props) => {
         <View style={styles.descriptionSynopsisContainer}>
           <Text style={styles.descriptionTitleText}>Synopsis</Text>
           <Text
-            numberOfLines={isLimitLine?2:20}
-            style={[styles.descriptionTitleText, {fontWeight: 'none'}]}>
+            numberOfLines={isLimitLine ? 2 : 20}
+            style={[styles.descriptionTitleText, {fontWeight: '100'}]}>
             {item.overview}
           </Text>
           <TouchableOpacity
             onPress={handlePress}
             style={{position: 'absolute', bottom: 0, right: 0}}>
-            <Text style={{color:'#c62828'}} >{isLimitLine ? 'Read More' : 'See Less'}</Text>
+            <Text style={{color: '#c62828'}}>
+              {isLimitLine ? 'Read More' : 'See Less'}
+            </Text>
           </TouchableOpacity>
         </View>
+        {isLoading && <LoadingSpinner />}
+        {detail && (
+          <ScrollView
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            snapToInterval={100}
+            contentContainerStyle={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginVertical: 30,
+            }}
+            pagingEnabled>
+            {detail?.production_companies?.map((company) => (
+              <View
+                style={{
+                  marginHorizontal: 50,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={{
+                    uri: `https://image.tmdb.org/t/p/original${company.logo_path}`,
+                  }}
+                  resizeMode={'stretch'}
+                  style={{width: 50, height: 50}}
+                />
+                <Text style={styles.descriptionTitleText} >{company.name}</Text>
+              </View>
+            ))}
+          </ScrollView>
+        )}
       </ScrollView>
     </>
   );
 };
 
+Description.sharedElements = (route, otherRoute, showing) => {
+  const {item} = route.params;
+  return [`item.${item.id}.image`];
+};
 export default Description;
 
 const styles = StyleSheet.create({
@@ -59,7 +134,7 @@ const styles = StyleSheet.create({
   },
   descriptionImage: {
     width,
-    height: height * 0.37,
+    height: height * 0.44,
   },
   descriptionTitleContainer: {
     flexGrow: 0,
@@ -71,7 +146,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 14,
     color: '#455A64',
-    textAlign:'justify'
+    textAlign: 'justify',
   },
   descriptionSynopsisContainer: {
     paddingVertical: 20,
