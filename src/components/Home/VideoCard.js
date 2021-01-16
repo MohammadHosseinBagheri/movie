0bb53a9bead3b74582839a2c229a44a9fec93332
MohammadHossein <5309489-mohammadhosseinbagheri@users.noreply.gitlab.com>
2021-01-16 23:22:53 +0330

    refactor

--- src/components/Home/VideoCard.js
+++ src/components/Home/VideoCard.js
@@ -9,89 +9,26 @@ import {
   Image,
   TouchableOpacity,
 } from 'react-native';
-import {SharedElement} from 'react-navigation-shared-element';
 import {useGridState} from '../../context/grid/Grid.provider';
-import {useNavigation} from '@react-navigation/native';
 import {useGetMovieDetails} from '../../hooks';
-
+import VideoImageContainer from './VideoImageContainer';
+import VideoTextContent from './VideoTextContent';
+import * as Animatable from 'react-native-animatable';
+const AnimationView = Animatable.createAnimatableComponent(View);
+const animation={
+  0:{translateY:80,opacity:0},
+  1:{translateY:0,opacity:1},
+}
 const {width, height} = Dimensions.get('window');
 const VideoCard = (props) => {
   const {item, index} = props;
   const gridStatus = useGridState();
-  const navigation = useNavigation();
   console.log(item.id);
   return (
-    <View style={styles.videoContainer}>
-      {!gridStatus && (
-        <View style={styles.videoSideDetailsContainer}>
-          <View style={styles.videoSideDetailsHeader}>
-            <Text>{item.title}</Text>
-            <Text>
-              {item.release_date} | {item.original_language}
-            </Text>
-          </View>
-          <View>
-            <Text>
-              {item.vote_average}{' '}
-              {
-                <Icon
-                  style={{fontSize: PixelRatio.getFontScale() * 19}}
-                  name="people-circle-outline"
-                />
-              }{' '}
-            </Text>
-            <Text>
-              {item.vote_count}{' '}
-              {
-                <Icon
-                  name="star-outline"
-                  style={{
-                    fontSize: PixelRatio.getFontScale() * 19,
-                    color: '#FFAB00',
-                  }}
-                />
-              }{' '}
-            </Text>
-          </View>
-        </View>
-      )}
-      <TouchableOpacity
-        onPress={() => {
-          navigation.push('Description', {item: item});
-        }}>
-        <TouchableOpacity
-          style={{
-            position: 'absolute',
-            right: 0,
-            top: 0,
-            zIndex: 100,
-            width: 50,
-            height: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Icon style={{color: '#546E7A'}} name="heart" />
-        </TouchableOpacity>
-        <SharedElement id={`item.${item.id}.image`}>
-          <Image
-            resizeMode={'stretch'}
-            source={{
-              uri: `https://image.tmdb.org/t/p/original${item.poster_path}`,
-            }}
-            style={{
-              justifyContent: 'space-between',
-              borderBottomLeftRadius: 30,
-              borderBottomRightRadius: 30,
-              borderTopLeftRadius: 30,
-              borderTopRightRadius: 30,
-              height: PixelRatio.getPixelSizeForLayoutSize(60),
-              width: PixelRatio.getPixelSizeForLayoutSize(50),
-              zIndex: 0,
-            }}
-          />
-        </SharedElement>
-      </TouchableOpacity>
-    </View>
+    <AnimationView  animation={animation} useNativeDriver delay={200} style={styles.videoContainer}>
+      {!gridStatus ? <VideoTextContent item={item} /> : null}
+      <VideoImageContainer item={item} />
+    </AnimationView>
   );
 };
 
@@ -107,11 +44,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row-reverse',
     padding: 2,
   },
-  videoSideDetailsContainer: {
-    marginHorizontal: 5,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-  },
+
   videoSideDetailsHeader: {},
   videoSideDetailsFooter: {},
 });
